66d2fc57c69c5d564ec64252a15438f82f634507
liusirdotnet <17611592337@163.com>
2019-09-10 15:38:28 +0800

    edit

--- src/FirstPackage.php
+++ src/FirstPackage.php
@@ -27,4 +27,20 @@ class FirstPackage
     {
         echo 'This is a test function';
     }
+
+    /**  
+    * This is a test function
+    * 
+    * @access public 
+    * @param string $arg1 参数一的说明 
+    * @param int    $arg2 参数二的说明 
+    * @param mixed  $mixed 这是一个混合类型 
+    * @return array 返回类型
+    * @author Name <email@email.com>
+    * @date 2018-06-17
+    */  
+    public function func2()
+    {
+        echo 'func2';
+    }
 }
